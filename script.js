c7c4d7e0068940c4ac3e405deab9c8caab81e3e6
Jendra Bayu <153905802+jendrabn@users.noreply.github.com>
2024-12-14 20:28:40 +0700

    Update

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+//Dropdowns func
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navbar-nav a");
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 50;
+    const sectionHeight = section.offsetHeight;
+    if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
+      current = section.getAttribute("id");
+    }
+  });
+  navLinks.forEach((link) => {
+    /*link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }*/
+  });
+});
